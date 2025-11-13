db05b83cd351097c2dea9bda5736f01e024c5b56
yusupovbunyod000-stack <yusupovbunyod000@gmail.com>
2025-11-13 16:22:19 +0500

    birinchi commit1

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bunyod</title>
+</head>
+<body>
+
+
 <?php
-echo "hello world";
+
+$x = 10;
+$y = 5;
+
+$yigindi = $x + $y;
+$ayirma = $x - $y;
+$kopaytma = $x * $y;
+$bolinma = $x / $y;
 
 
-
+echo "Yig‘indi = $yigindi <br>";
+echo "Ayirma = $ayirma <br>";
+echo "Ko‘paytma = $kopaytma <br>";
+echo "Bo‘linma = $bolinma <br>";
 ?>
+
+</body>
+</html>
